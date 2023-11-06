6aaac84eaebce6d41332f5eff4f3920362bfbe6a
JojoPanda99 <jojo@pop-os.localdomain>
2023-11-05 22:58:41 -0300

    create migration

---
+++ src/ConfitecAPIData/Migrations/20231106015508_RenameTable.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ConfitecAPIData.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Users",
+                table: "Users");
+
+            migrationBuilder.RenameTable(
+                name: "Users",
+                newName: "Usuarios");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Surname",
+                table: "Usuarios",
+                type: "varchar(255)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(125)",
+                oldMaxLength: 125);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Usuarios",
+                type: "varchar(255)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(125)",
+                oldMaxLength: 125);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Usuarios",
+                type: "varchar(255)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(255)",
+                oldMaxLength: 255);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Usuarios",
+                table: "Usuarios",
+                column: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Usuarios",
+                table: "Usuarios");
+
+            migrationBuilder.RenameTable(
+                name: "Usuarios",
+                newName: "Users");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Surname",
+                table: "Users",
+                type: "nvarchar(125)",
+                maxLength: 125,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(255)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Users",
+                type: "nvarchar(125)",
+                maxLength: 125,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(255)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Users",
+                type: "nvarchar(255)",
+                maxLength: 255,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(255)");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Users",
+                table: "Users",
+                column: "Id");
+        }
+    }
+}
