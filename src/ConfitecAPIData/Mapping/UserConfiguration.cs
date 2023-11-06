fff191b28ab85584c0cd7f6364f9b6c6752b3d22
JojoPanda99 <jojo@pop-os.localdomain>
2023-11-05 22:47:20 -0300

    Change Table name

--- src/ConfitecAPIData/Mapping/UserConfiguration.cs
+++ src/ConfitecAPIData/Mapping/UserConfiguration.cs
@@ -9,10 +9,12 @@ public class UserConfiguration : IEntityTypeConfiguration<User>
     public void Configure(EntityTypeBuilder<User> builder)
     {
         builder.HasKey(u => u.Id);
-        builder.Property(u => u.Name).IsRequired().HasMaxLength(255);
-        builder.Property(u => u.Surname).IsRequired().HasMaxLength(255);
-        builder.Property(u => u.Email).IsRequired().HasMaxLength(255);
+        builder.Property(u => u.Name).IsRequired().HasColumnType("varchar(255)");
+        builder.Property(u => u.Surname).IsRequired().HasColumnType("varchar(255)");
+        builder.Property(u => u.Email).IsRequired().HasColumnType("varchar(255)");
         builder.Property(u => u.BirthDate).IsRequired();
         builder.Property(u => u.Education).HasConversion<int>().IsRequired();
+
+        builder.ToTable("Usuarios");
     }
 }
